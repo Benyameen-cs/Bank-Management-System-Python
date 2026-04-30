16188b6de9e61ad37061f2ba92a8f2182d9b8b20
Benyameen-cs <benyameen009@gmail.com>
2026-04-30 21:51:48 +0500

    update the main file adding main() in it

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from accounts import create_account , del_account , check_balance
 from transcations import deposit , withdraw
  
 
-bank_open = True
-
-
 def create_bank_account():   
     print('Enter your details to create your bank account')
     name  = input('Enter your name : ')
@@ -39,32 +36,37 @@ def delele_bank_account():
     name = input('Enter your name :')
     password = input('Enter your password :')
     del_account(name,password)
-    
 
-while bank_open:
-    print("Enter any options to continue \nBank is open")
-    print("1.To create bank account \n2.To check balance " \
-    "\n3.To deposit money \n4.To withdraw money " \
-    "\n5.To delete account \n6.To close bank")
-    option = int(input('Enter the option : '))
-    match option:
-        case 1 :
-            create_bank_account()
-        case 2 :
-            check_bank_bal()
-        case 3 :
-            deposit_money()
-        case 4 :
-            withdraw_money()
-        case 5 :
-            delele_bank_account()
-        case 6 :
-            bank_open = False
-        case _ :
-            print("option not matches")
-    
 
-print("Bank is closed")
+def main():
+    bank_open = True
+
+    while bank_open:
+        print("Enter any options to continue \nBank is open")
+        print("1.To create bank account \n2.To check balance " \
+        "\n3.To deposit money \n4.To withdraw money " \
+        "\n5.To delete account \n6.To close bank")
+        option = int(input('Enter the option : '))
+        match option:
+            case 1 :
+                create_bank_account()
+            case 2 :
+                check_bank_bal()
+            case 3 :
+                deposit_money()
+            case 4 :
+                withdraw_money()
+            case 5 :
+                delele_bank_account()
+            case 6 :
+                bank_open = False
+            case _ :
+                print("option not matches")        
+    print("Bank is closed")
+
+
+if __name__ == '__main__':
+    main()
 
 
 
